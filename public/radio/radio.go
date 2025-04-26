14840b85abeb4c013da511899524f9abd513f157
rabarar <robertbaruch@mac.com>
2025-04-26 10:23:47 -0400

    format as string

--- public/radio/radio.go
+++ public/radio/radio.go
@@ -102,7 +102,7 @@ func TryDecode(packet *meshtastic.MeshPacket, key []byte) (*meshtastic.Data, err
 			log.Warnf("Failed decrypting packet: %s", err)
 			return nil, ErrDecrypt
 		}
-		log.Warnf("decrypted packet: [%s]", decrypted)
+		log.Warnf("decrypted packet: [%s]", string(decrypted))
 
 		var meshPacket meshtastic.Data
 		err = proto.Unmarshal(decrypted, &meshPacket)
